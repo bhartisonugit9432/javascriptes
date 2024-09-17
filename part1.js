90156dcf778dde71656602e8bed1d6721aa2ee1e
Sonu bharti <154326882+bhartisonugit9432@users.noreply.github.com>
2024-09-17 09:09:20 +0000

    git config

--- part1.js
+++ part1.js
@@ -2,9 +2,10 @@ const FindLongestWord = (str) => {
   if ((str.trim().length = 0)) {
     return false;
   }
-  words = str.split("");
-  words = words.sort();
+  words = str.split(" ");
+  words = words.sort((a,b)=>b.length-a.length);
   console.log(words);
+  return words [0];
 };
 console.log(
   FindLongestWord("thing doesn't work. Anyone know how to go about this")
